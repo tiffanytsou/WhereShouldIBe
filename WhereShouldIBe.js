6ff27b561888ca66ab76ebec0720f2c5ccef7455
tiffanytsou <tsoutiffany@gmail.com>
2014-06-24 15:17:52 -0400

    Update WhereShouldIBe.js

--- WhereShouldIBe.js
+++ WhereShouldIBe.js
@@ -1,10 +1,10 @@
 //comparing time
 var x = new Date();
-x.setFullYear(2100, 0, 14);
+x.setFullYear(2014, 5, 20);
 var today = new Date();
 
 if (x > today) {
-    alert("Today is before 14th January 2100");
+    alert("Today is before 20th June 2014");
 } else {
-    alert("Today is after 14th January 2100");
+    alert("Today is after 20th June 2014");
 }
