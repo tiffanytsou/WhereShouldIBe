d521c90e8cbbfdbcbeacde42282a57ee8dff8ffc
tiffanytsou <tsoutiffany@gmail.com>
2014-06-24 15:13:20 -0400

    Update WhereShouldIBe.js

--- WhereShouldIBe.js
+++ WhereShouldIBe.js
@@ -1,23 +1,10 @@
-//curent date
-	var currentDate = new Date()
-	var day = currentDate.getDate()
-	var month = currentDate.getMonth() + 1
-	var year = currentDate.getFullYear()
-	document.write("<b>" + day + "/" + month + "/" + year + "</b>")
+//comparing time
+var x = new Date();
+x.setFullYear(2100, 0, 14);
+var today = new Date();
 
-//current time
-	var currentTime = new Date()
-	var hours = currentTime.getHours()
-	var minutes = currentTime.getMinutes()
-
-	if (minutes < 10)
-	minutes = "0" + minutes
-
-	var suffix = "AM";
-	if (hours >= 12) {
-	suffix = "PM";
-	hours = hours - 12;
-	}
-	if (hours == 0) {
-	hours = 12;
-	}
+if (x > today) {
+    alert("Today is before 14th January 2100");
+} else {
+    alert("Today is after 14th January 2100");
+}
