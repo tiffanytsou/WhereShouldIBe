62e43ec27a7d4fd04533f7c0ba4fb74ef640f24b
tiffanytsou <tsoutiffany@gmail.com>
2014-06-26 11:06:44 -0400

    Update WhereShouldIBe.js

--- WhereShouldIBe.js
+++ WhereShouldIBe.js
@@ -6,13 +6,4 @@ var x;
     }
     document.getElementById("demo").innerHTML = x;
 }
-//comparing time
-var x = new Date();
-x.setFullYear(2014, 5, 20);
-var today = new Date();
 
-if (x > today) {
-    alert("Today is before 20th June 2014");
-} else {
-    alert("Today is after 20th June 2014");
-}
