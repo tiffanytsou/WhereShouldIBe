bcc1a7a0144a8745a50c4da57728ef6e0c75cb2e
tiffanytsou <tsoutiffany@gmail.com>
2014-06-26 11:27:23 -0400

    Update WhereShouldIBe.js

--- WhereShouldIBe.js
+++ WhereShouldIBe.js
@@ -1,9 +1,11 @@
-var x;
-    if (confirm("Current Date") == true) {
-        x = "After";
-    } else {
-        x = "Before";
-    }
+var x = new Date();
+x.setFullYear(2100, 0, 14);
+var today = new Date();
+
+if (x > today) {
+    alert("Today is before 14th January 2100");
+} else {
+    alert("Today is after 14th January 2100");
     document.getElementById("demo").innerHTML = x;
 }
 
