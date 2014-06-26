9d158fd84df87bd860577fa64504b24b8098d836
tiffanytsou <tsoutiffany@gmail.com>
2014-06-26 11:48:34 -0400

    Update WhereShouldIBe.js

--- WhereShouldIBe.js
+++ WhereShouldIBe.js
@@ -2,18 +2,11 @@ var x = new Date();
 x.setFullYear(2100, 0, 14);
 var today = new Date();
 
-if (x > today) {
-    alert("Today is before 14th January 2100");
-} else {
-    alert("Today is after 14th January 2100");
-}
-
 function myFunction() {
 var x;
-    if (confirm("Press a button!") == true) {
-        x = "You pressed OK!";
+    if (confirm("Press a button!")) === x > today {
+    alert("Today is after 14th January 2100");
     } else {
-        x = "You pressed Cancel!";
-    }
+    alert("Today is after 14th January 2100");
     document.getElementById("demo").innerHTML = x;
 }
