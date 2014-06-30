51efbbbc00602d82308d70aba598204c9603f201
tiffanytsou <tsoutiffany@gmail.com>
2014-06-30 08:59:43 -0400

    Update WhereShouldIBe.js

--- WhereShouldIBe.js
+++ WhereShouldIBe.js
@@ -1,13 +1,22 @@
-var time = new Date();
-console.log(time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
-document.getElementById("time").innerHTML = x;
+var x = new Date();
+x.setFullYear(2100, 0, 14);
+var today = new Date();
 
-function myFunction() {
-    var x;
-    if (confirm("Press a button!") > today) {
-        x = "After";
-    } else {
-        x = "Before";
-    }
-    document.getElementById("demo").innerHTML = x;
+if (x > today) {
+    alert("Today is before 14th January 2100");
+} else {
+    alert("Today is after 14th January 2100");
+}
+
+
+document.getElementById("current").innerHTML = formatAMPM();
+
+function formatAMPM() {
+var d = new Date(),
+    minutes = d.getMinutes().toString().length == 1 ? '0'+d.getMinutes() : d.getMinutes(),
+    hours = d.getHours().toString().length == 1 ? '0'+d.getHours() : d.getHours(),
+    ampm = d.getHours() >= 12 ? 'pm' : 'am',
+    months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+    days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+return days[d.getDay()]+' '+months[d.getMonth()]+' '+d.getDate()+' '+d.getFullYear()+' '+hours+':'+minutes+ampm;
 }
