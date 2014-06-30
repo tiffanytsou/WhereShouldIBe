2671ad3c83ef7442751e1fe6d2742d5a6c3b190a
tiffanytsou <tiffany.tsou@careerbuilder.com>
2014-06-30 09:16:55 -0400

    Revert "Update WhereShouldIBe.js"
    This reverts commit a6024fa41cd0508a17799d70bdcea9f1c25d3de4.

--- WhereShouldIBe.js
+++ WhereShouldIBe.js
@@ -25,3 +25,20 @@ function showPosition(position) {
     +latlon+"&zoom=14&size=400x300&sensor=false";
     document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
 }
+
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            x.innerHTML = "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            x.innerHTML = "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            x.innerHTML = "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            x.innerHTML = "An unknown error occurred."
+            break;
+    }
+}
