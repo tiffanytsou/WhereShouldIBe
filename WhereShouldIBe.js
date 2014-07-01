512a7afeb019fc4a7969a03eec74410da5c5d1d2
tiffanytsou <tsoutiffany@gmail.com>
2014-07-01 09:53:14 -0400

    Update WhereShouldIBe.js

--- WhereShouldIBe.js
+++ WhereShouldIBe.js
@@ -1,15 +1,3 @@
-var x = new Date();
-x.setFullYear(2100, 0, 14);
-var today = new Date();
-
-if (x > today) {
-    alert("Today is before 14th January 2100");
-} else {
-    alert("Today is after 14th January 2100");
-}
-
-var x = document.getElementById("demo");
-
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition,showError);
