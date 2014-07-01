8cfed8c81313c3a5b1fc1f71aedbcfe873b49e92
tiffanytsou <tsoutiffany@gmail.com>
2014-07-01 09:46:09 -0400

    Update WhereShouldIBe.js

--- WhereShouldIBe.js
+++ WhereShouldIBe.js
@@ -80,12 +80,152 @@ class Schedule {
         locations[14] = "work";
         locations[15] = "work";
         locations[16] = "home";
+        locations[17] = "home";
+        locations[18] = "home";
+        locations[19] = "home";
+        locations[20] = "home";
+        locations[21] = "home";
+        locations[22] = "home";
+        locations[23] = "home";
+        locations[24] = "home";
+    }
+    private void loadTuesday() {
+        locations[0] = "home";
+        locations[1] = "home";
+        locations[2] = "home";
+        locations[3] = "home";
+        locations[4] = "home";
+        locations[5] = "home";
+        locations[6] = "home";
+        locations[7] = "home";
+        locations[8] = "home";
+        locations[9] = "work";
+        locations[10] = "work";
+        locations[11] = "work";
+        locations[12] = "work";
+        locations[13] = "work";
+        locations[14] = "work";
+        locations[15] = "work";
         locations[16] = "home";
+        locations[17] = "home";
+        locations[18] = "taekwondo";
+        locations[19] = "home";
+        locations[20] = "home";
+        locations[21] = "home";
+        locations[22] = "home";
+        locations[23] = "home";
+        locations[24] = "home";
+    }
+    private void loadWednesday() {
+        locations[0] = "home";
+        locations[1] = "home";
+        locations[2] = "home";
+        locations[3] = "home";
+        locations[4] = "home";
+        locations[5] = "home";
+        locations[6] = "home";
+        locations[7] = "home";
+        locations[8] = "home";
+        locations[9] = "home";
+        locations[10] = "tutoring";
+        locations[11] = "tutoring";
+        locations[12] = "tutoring";
+        locations[13] = "home";
+        locations[14] = "home";
+        locations[15] = "home";
         locations[16] = "home";
-        locations[16] = "home";
-        locations[16] = "home";
-        locations[16] = "home";
-        locations[16] = "home";
-        locations[16] = "home";
+        locations[17] = "home";
+        locations[18] = "home";
+        locations[19] = "home";
+        locations[20] = "home";
+        locations[21] = "home";
+        locations[22] = "home";
+        locations[23] = "home";
+        locations[24] = "home";
+    }
+    private void loadThursday() {
+        locations[0] = "home";
+        locations[1] = "home";
+        locations[2] = "home";
+        locations[3] = "home";
+        locations[4] = "home";
+        locations[5] = "home";
+        locations[6] = "home";
+        locations[7] = "home";
+        locations[8] = "home";
+        locations[9] = "work";
+        locations[10] = "work";
+        locations[11] = "work";
+        locations[12] = "work";
+        locations[13] = "work";
+        locations[14] = "work";
+        locations[15] = "work";
+        locations[16] = "green tea";
+        locations[17] = "green tea";
+        locations[18] = "green tea";
+        locations[19] = "green tea";
+        locations[20] = "green tea";
+        locations[21] = "home";
+        locations[22] = "home";
+        locations[23] = "home";
+        locations[24] = "home";
+    }
+    private void loadFriday() {
+        locations[0] = "home";
+        locations[1] = "home";
+        locations[2] = "home";
+        locations[3] = "home";
+        locations[4] = "home";
+        locations[5] = "home";
+        locations[6] = "home";
+        locations[7] = "home";
+        locations[8] = "home";
+        locations[9] = "home";
+        locations[10] = "home";
+        locations[11] = "home";
+        locations[12] = "home";
+        locations[13] = "home";
+        locations[14] = "home";
+        locations[15] = "green tea";
+        locations[16] = "green tea";
+        locations[17] = "green tea";
+        locations[18] = "green tea";
+        locations[19] = "green tea";
+        locations[20] = "green tea";
+        locations[21] = "home";
+        locations[22] = "home";
+        locations[23] = "home";
+        locations[24] = "home";
+    }
+    private void loadSaturday() {
+        locations[0] = "home";
+        locations[1] = "home";
+        locations[2] = "home";
+        locations[3] = "home";
+        locations[4] = "home";
+        locations[5] = "home";
+        locations[6] = "home";
+        locations[7] = "home";
+        locations[8] = "home";
+        locations[9] = "home";
+        locations[10] = "home";
+        locations[11] = "home";
+        locations[12] = "home";
+        locations[13] = "tutoring";
+        locations[14] = "tutoring";
+        locations[15] = "tutoring";
+        locations[16] = "green tea";
+        locations[17] = "green tea";
+        locations[18] = "green tea";
+        locations[19] = "green tea";
+        locations[20] = "green tea";
+        locations[21] = "home";
+        locations[22] = "home";
+        locations[23] = "home";
+        locations[24] = "home";
+    }
+    private void loadSunday() {
+        locations[0] = "home";
+        locations[0] = "home";
     }
 }
